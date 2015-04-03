9f96e431f9755bff3f150d9f0f04821ac7e3261a
Mike Loffland <mloffland@outlook.com>
2015-04-03 15:00:49 -0500

    Inital commit

---
+++ src/app.js
@@ -0,0 +1,38 @@
+(function () {
+
+  angular.module('app', ['mapbox-forward-geo'])
+
+    .controller('AppCtrl', function($scope){
+      $scope.myapitoken = 'pk.eyJ1IjoibWxvZmZsYW5kIiwiYSI6Ik5leC11NlUifQ.h2UgWXhT5l7zjts894SySw';
+
+      //$scope.mapboxoptions = {
+      //  placeHolderText: 'Search Address, City or Zip',
+      //  autoSuggest: true,
+      //  excludeEntriesWithNoPlaceName: true,
+      //  minLength: 3,
+      //  minLengthErrorText: 'Please enter at least %N% character(s).',
+      //  includeThisKeyword: 'oklahoma'
+      //};
+
+
+      // watch the addressSelection model
+      $scope.$watchCollection('addressSelection',function(){
+        // spit it out to the console
+        if(angular.isDefined($scope.addressSelection)){
+          console.log('Selected address/city/zip info');
+          console.log($scope.addressSelection);
+        }
+      });
+
+
+      // watch the addressSelection model
+      $scope.$watchCollection('results',function(){
+        // spit it out to the console
+        if(angular.isDefined($scope.results)){
+          console.log('results');
+          console.log($scope.results);
+        }
+      });
+  });
+
+}());
